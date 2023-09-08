4e737d02684fe6f7d47f5c5ed47028d7b4869adf
Abhishek <30773432+CGA21@users.noreply.github.com>
2023-09-08 01:28:43 -0500

    Add files via upload
    first upload

---
+++ AdMobViewRenderer.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Gms.Ads;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using App1;
+using App1.Droid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly:ExportRenderer(typeof(AdMobView),typeof(AdMobViewRenderer))]
+namespace App1.Droid
+{
+    public class AdMobViewRenderer : ViewRenderer<AdMobView, AdView>
+    {
+        public AdMobViewRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<AdMobView> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null)
+            {
+                var adView = new AdView(Context)
+                {
+                    AdUnitId = "ca-app-pub-2165341686632098/1176174976",
+                    AdSize = AdSize.Banner,
+                    
+                };
+                adView.LayoutParameters = new LayoutParams(LayoutParams.MatchParent, LayoutParams.MatchParent);
+                AdRequest.Builder adRequest = new AdRequest.Builder();
+                adView.LoadAd(adRequest.Build());
+                SetNativeControl(adView);
+            }
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.Gms.Ads;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+
+namespace App1.Droid
+{
+    [Activity(Label = "Tambola!!", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+//          Used for Testing Ads. Disable before Main Upload
+/*            RequestConfiguration.Builder builder = new RequestConfiguration.Builder();
+            builder = builder.SetTestDeviceIds(new string[] { "9FAE4AB68FB1BB65C7ED6DA68447147F" });
+            MobileAds.RequestConfiguration = builder.Build();
+*/
+            MobileAds.Initialize(ApplicationContext);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+    }
+}
